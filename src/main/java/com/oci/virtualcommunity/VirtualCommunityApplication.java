1ae3654c12405198862601ec323e438246885737
赵林杰 <17832927143@htwy.com>
2024-10-20 15:13:33 +0800

    项目初始化

---
+++ src/main/java/com/oci/virtualcommunity/VirtualCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.oci.virtualcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oci/virtualcommunity/VirtualCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oci.virtualcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
